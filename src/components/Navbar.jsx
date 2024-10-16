98fbc12847ea850f1f5bfa19cc9329d26c016b8a
prasad <prasad.lakhara@eroam.com>
2024-10-16 12:35:43 +0530

    Navbar Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,6 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
             Prasad &nbsp;
-            <span className="sm:block hidden"> | JS Mastery</span>
           </p>
         </Link>
 
